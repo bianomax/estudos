d9a7a53cf5ec39326d11c6230cfce48b8a337591
Fabiano Maximiano <biano@live.com>
2024-06-11 19:30:17 -0300

    commit final do dia

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ let numeroSecreto = 15;
 
 numeroAposta = prompt(nomeJogador + ' Escolha um numero de 1 á 30');
 
+//Se o numero secreto for igual ao numero aposta.
+//Mostraremos a uma mensagem informando ao acerto.
 if(numeroSecreto == numeroAposta){
     console.log(`${nomeJogador} você acertou o numero secreto ${numeroSecreto}`);
 }else{
+    //Senão mostramos uma mensagem de erro
     console.log(`${nomeJogador} você errou o numero secreto !!!`);
 }
