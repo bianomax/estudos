8bff75b7193a9bf1190c2d00c5f6d80c335f66b5
Fabiano Maximiano <biano@live.com>
2024-06-11 14:56:39 -0300

    informando e criando variaveis

---
+++ app.js
@@ -0,0 +1,5 @@
+let nomeJogador = prompt('Informe o nome do jogador: ');
+alert('Seja bem vindo ' + nomeJogador + ' ao jogo do número secreto!!');
+let numeroSecreto = 15;
+
+prompt(nomeJogador + ' Escolha um numero de 1 á 30');
