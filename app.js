45f86503f7d9f9d042c6415b810815b7b7c275ad
Fabiano Maximiano <biano@live.com>
2024-06-11 16:25:48 -0300

    informando novas maneiras de desenvolver o codigo

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ let nomeJogador = prompt('Informe o nome do jogador: ');
 alert('Seja bem vindo ' + nomeJogador + ' ao jogo do número secreto!!');
 let numeroSecreto = 15;
 
-prompt(nomeJogador + ' Escolha um numero de 1 á 30');
+numeroAposta = prompt(nomeJogador + ' Escolha um numero de 1 á 30');
+
+if(numeroSecreto == numeroAposta){
+    console.log(`${nomeJogador} você acertou o numero secreto ${numeroSecreto}`);
+}else{
+    console.log(`${nomeJogador} você errou o numero secreto !!!`);
+}
