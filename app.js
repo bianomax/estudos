3320a8e5b8d78071042c5e8ed9bff229c90daf3c
Fabiano Maximiano <biano@live.com>
2024-09-03 13:06:31 -0300

    incluindo novas condicionais para testar o jogo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let nomeJogador = prompt('Informe o nome do jogador: ');
 alert('Seja bem vindo ' + nomeJogador + ' ao jogo do número secreto!!');
-let numeroSecreto = 15;
+let numeroSecreto = 3;
 
 numeroAposta = prompt(nomeJogador + ' Escolha um numero de 1 á 30');
 
@@ -10,5 +10,10 @@ if(numeroSecreto == numeroAposta){
     console.log(`${nomeJogador} você acertou o numero secreto ${numeroSecreto}`);
 }else{
     //Senão mostramos uma mensagem de erro
-    console.log(`${nomeJogador} você errou o numero secreto !!!`);
+    //console.log(`${nomeJogador} você errou o numero secreto !!!`);
+    if(numeroSecreto > numeroAposta){
+        console.log(`O numero secreto é menor que ${numeroAposta}`);
+    }else{
+        console.log(`O numero secreto é maior que ${numeroAposta}`);
+    }
 }
